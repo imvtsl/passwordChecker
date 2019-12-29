a5a4ac4124ca40aac12694c4e658d58d2b525562
imvtsl <vatsalrathi@outlook.com>
2019-12-29 14:28:46 +0530

    Design changes and password manipulator added.

--- main.cpp
+++ main.cpp
@@ -9,15 +9,44 @@ string encrypted(string password)
 	return password;
 }
 
-string manipulatePassword(string newPassword, int toleranceLevel)
+void manipulateHelper(string password, int toleranceLevel, vector<string> &result, string prefix)
 {
-	string manipulatedPassword = newPassword;
-	int passwordSize = newPassword.size();
-	// choose a character
-	// choose a character from remaining string until tolerance level is 0.
-	// manipulate character at these indices.
+	if(toleranceLevel == 0)
+	{
+		password.insert(0, prefix);
+		result.push_back(password);
+		return;
+	}
+	//vector<string> result;
+	int stringSize = password.size();
+	for(int i=0 ; i<=stringSize-toleranceLevel ; i++)
+	{
+		char temp = password[i];
+		int substrLen = stringSize-i-1;
+		int traversedLen = stringSize - substrLen;
 
-	return manipulatedPassword;
+		for(int ascii = 32 ; ascii<=126 ; ascii++)
+		{
+			char c = ascii;
+			password[i] = c;
+			// make new prefix
+			
+
+			string appendedPrefix = password.substr(0,traversedLen);
+			string newPrefix = prefix;
+			newPrefix.append(appendedPrefix);
+		
+			manipulateHelper(password.substr(i+1, substrLen), toleranceLevel-1, result, newPrefix);
+		}
+		password[i] = temp;
+	}
+	return;
+}
+
+void manipulatePassword(string password, int toleranceLevel, vector<string> &result)
+{
+	manipulateHelper(password, toleranceLevel, result, "");
+	return;
 }
 
 bool isDuplicate(string newPassword)
@@ -36,57 +65,71 @@ bool isDuplicate(string newPassword)
 	return result;
 }
 
-int checkTolerance(string newPassword, string oldPassword, int toleranceOfCharacters)
+int checkTolerance(string newPassword, string oldPassword, int toleranceOfSimilarity)
 {
-	int result = -1;
+	int tolerance = -1;
 	// assume passDb has encrypted password.
 	if(isDuplicate(newPassword))
 		return 0;
 
-	for(int toleranceLevel = 1 ; toleranceLevel <= toleranceOfCharacters ; toleranceLevel++)
+	bool duplicate = false;
+	
+	for(int toleranceLevel = 1 ; toleranceLevel <= toleranceOfSimilarity ; toleranceLevel++)
 	{
 		
 		// pick toleranceLevel number of characters, change them, and verify.
 		
 		// function to pick toleranceLevel number of characters.
 		// function to manipulate them.
-		string manipulatedPassword = manipulatePassword(newPassword, toleranceLevel);
-		cout << "Manipulated password is: " << manipulatedPassword << endl;
-		// function to check.
-		if(isDuplicate(manipulatedPassword)) {
-			result = toleranceLevel;
-			break;
+		vector<string> result;
+		manipulatePassword(password, toleranceLevel, result);
+		
+		for(int i=0 ; i<=result.size()-1 ; i++)
+		{
+			if(isDuplicate(result[i]))
+			{
+				tolerance = toleranceLevel;
+				duplicate = true;
+				break;
+			}	
 		}
+		if(duplicate)
+			break;
 	}
-	return result;
+	return tolerance;
 }
 
 int main() {
-	// should password checked be in continuous loop?
+	
 	char oldPassword[20];
 	char newPassword[20];
 	char confirmNewPassword[20];
 	
-	cin.getline(oldPassword, 20);
-	cin.getline(newPassword, 20);
-	cin.getline(confirmNewPassword, 20);
+	while(true)
+	{
+		cin.getline(oldPassword, 20);
+		cin.getline(newPassword, 20);
+		cin.getline(confirmNewPassword, 20);
 
-	cout << "Entered old password is: " << oldPassword << endl;
-	cout << "Entered new password is: " << newPassword << endl;
-	cout << "Entered confirm password is: " << confirmNewPassword << endl;
+		cout << "Entered old password is: " << oldPassword << endl;
+		cout << "Entered new password is: " << newPassword << endl;
+		cout << "Entered confirm password is: " << confirmNewPassword << endl;
 	
-	// make basic checks on password;
+		// make basic checks on password;
 	
-	// check if old password is correct
-	// check if newPassword and confirmPassword match
-	// check if newPassword satifies criteria for strong password
-	// check if new password is tolerably distinct from old passwords.
-	const int toleranceOfCharacters = 3;
-	int tolerance = checkTolerance(newPassword, oldPassword, toleranceOfCharacters);
-	if(tolerance != -1)
-		cout << "More than " << tolerance << " characters should be different than previous passwords." << endl;
-	else
-		cout << "Password changed successfully." << endl;
+		// check if old password is correct
+		// check if newPassword and confirmPassword match
+		// check if newPassword satifies criteria for strong password
+		// check if new password is tolerably distinct from old passwords.
+		const int toleranceOfSimilarity = 3;
+		int tolerance = checkTolerance(newPassword, oldPassword, toleranceOfSimilarity);
+		if(tolerance != -1)
+			cout << "More than " << toleranceOfSimilarity << " characters should be different than previous passwords." << endl;
+		else
+		{
+			cout << "Password changed successfully." << endl;
+			break;
+		}
+	}
 	return 0;
-
 }
